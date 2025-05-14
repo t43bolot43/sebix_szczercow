7161f10b25fc0c78212486fc61f0b29afdcc3ee6
t43bolot43 <t43bolot44@gmail.com>
2025-05-14 13:17:39 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -2,20 +2,17 @@ var div = document.getElementById('main');
 var lewo = document.getElementById('lewo');
 var prawo = document.getElementById('prawo');
 var i = 1;
-
 console.log(prawo);
-console.log(lewo);
-lewo.addEventListener("click", odejmij);
+console.log(lewo)
 
+lewo.addEventListener("click", odejmij);
 prawo.addEventListener("click", dodaj);
 
 function odejmij(){
     i = i - 1;
+    div.innerHTML = "<img src="+ i + "'.jpeg'/>";
 }
 function dodaj(){
     i = i + 1;
-}
-
-while(True){
-    div.innerHTML = "<img src="+ i + "'.jpeg'/>";
+    div.innerHTML = "<img src='" + i + ".jpeg'/>";
 }
