d10767f98a54c20b36fac5af4eb2853f8313f833
t43bolot43 <t43bolot44@gmail.com>
2025-05-14 13:22:20 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,4 +1,5 @@
 var div = document.getElementById('main');
+var div2 = document.getElementById('obrazek');
 var lewo = document.getElementById('lewo');
 var prawo = document.getElementById('prawo');
 var i = 1;
@@ -10,11 +11,11 @@ prawo.addEventListener("click", dodaj);
 
 function odejmij(){
     i = i - 1;
-    div.innerHTML = "<button id="lewo">Poprzedni</button>
-        <button id="prawo">Następny</button><img src="+ i + "'.jpeg'/>";
+    div.innerHTML = "<button id='lewo'>Poprzedni</button><button id='prawo'>Następny</button>";
+    div2.innerHTML = "<img src'" + i + ".jpeg'/>";
 }
 function dodaj(){
     i = i + 1;
-    div.innerHTML = "<button id="lewo">Poprzedni</button>
-        <button id="prawo">Następny</button><img src='" + i + ".jpeg'/>";
+    div.innerHTML = "<button id='lewo'>Poprzedni</button><button id='prawo'>Następny</button>";
+    div2.innerHTML = "<img src'" + i + ".jpeg'/>";
 }
