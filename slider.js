62c4b51bb6812868a20e106c8f19f10667413808
t43bolot43 <t43bolot44@gmail.com>
2025-05-14 13:01:16 +0200

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,18 @@
+var div = document.getElementById('main');
+var lewo = document.getElementById('lewo');
+var prawo = document.getElementById('prawo');
+var i = 1;
+
+lewo.addEventListener("click", odejmij)
+prawo.addEventListener("click", dodaj)
+
+function odejmij(){
+    i = i - 1;
+}
+function dodaj(){
+    i = i + 1;
+}
+
+while(True){
+    div.innerHTML = "<img src="+ i + "'.jpeg'/>";
+}
